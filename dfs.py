4bbabcd056a0ae72a3fa617f17939e22350a7b2e
jeevasri905-beep <jeevasri.905@gmail.com>
2026-02-23 10:26:37 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,23 +1,22 @@
-
-
-'''Depth First Search uses STACK AND RECURSION
-'''
-#import defaultdict
+'''Depth First Search uses STACK AND RECURSION'''
 from collections import defaultdict
-def dfs(graph,start,visited,path):
-    path.append(start)
-    visited[start]=True
-    for neighbour in graph[start]:
-       #type ur code here
-    return path
-graph=defaultdict(list)
-n,e=map(int,input().split())
+    def dfs(graph, start, visited, path):
+        path.append(start)
+        visited[start] = True
+        for neighbour in graph[start]:
+            if not visited[neighbour]:   # check if neighbour is not visited
+                dfs(graph, neighbour, visited, path)   # recursive call
+        return path
+
+graph = defaultdict(list)
+n, e = map(int, input().split())   # number of nodes and edges
 for i in range(e):
-   #type ur code here
-    graph[v].append(u)
-#print(graph)
-start='A'
-visited=defaultdict(bool)
- #type ur code here
-traversedpath=dfs(graph,start,visited,path)
+    u, v = input().split()         # take edge input
+    graph[u].append(v)             # add edge u -> v
+    graph[v].append(u)             # add edge v -> u (if undirected)
+
+start = 'A'
+visited = defaultdict(bool)
+path = []                          # initialize path list
+traversedpath = dfs(graph, start, visited, path)
 print(traversedpath)
